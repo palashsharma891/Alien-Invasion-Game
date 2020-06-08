24cce740984bce3cb4a830151849da8d5e2ca805
Palash Sharma <54630543+palashsharma891@users.noreply.github.com>
2020-06-08 17:18:24 +0530

    Update alien.py

--- alien.py
+++ alien.py
@@ -1,90 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  8 16:36:38 2020
+Created on Mon Jun  8 14:40:11 2020
 
 @author: Palash
 """
 
-import pygame.font
-from pygame.sprite import Group
-from ship import Ship
+import pygame
+from pygame.sprite import Sprite
 
-class Scoreboard:
-    """A class to report scoring information."""
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+    
     def __init__(self, ai_game):
-        
-        """Initialize scorekeeping attributes."""
-        self.ai_game = ai_game
+        """Initialize the alien and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
-        self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
-        self.stats = ai_game.stats
-
-        # Font settings for scoring information.
-        self.text_color = (30, 30, 30)
-        self.font = pygame.font.SysFont(None, 48)
-
-        # Prepare the initial score images.
-        self.prep_score()
-        self.prep_high_score()
-        self.prep_level()
-        self.prep_ships()
         
-    def prep_score(self):
-        """Turn the score into a rendered image."""
-        rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
-
-        self.score_image = self.font.render(score_str, True,
-                                            self.text_color, self.settings.bg_color)
-
-        # Display the score at the top right of the screen.
-        self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rect.right - 20
-        self.score_rect.top = 20
+        # Load the alien image and set its rect attribute.
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
         
-    def prep_high_score(self):
-        """Turn the high score into a rendered image."""
-        high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True, 
-                                                 self.text_color, self.settings.bg_color)
-
-        # Center the high score at the top of the screen.
-        self.high_score_rect = self.high_score_image.get_rect()
-        self.high_score_rect.centerx = self.screen_rect.centerx
-        self.high_score_rect.top = self.score_rect.top
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
         
-    def prep_level(self):
-        """Turn the level into a rendered image."""
-        level_str = str(self.stats.level)
-        self.level_image = self.font.render(level_str, True,
-                                            self.text_color, self.settings.bg_color)
-
-        # Position the level below the score.
-        self.level_rect = self.level_image.get_rect()
-        self.level_rect.right = self.score_rect.right
-        self.level_rect.top = self.score_rect.bottom + 10
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
         
-    def prep_ships(self):
-        """Show how many ships are left."""
-        self.ships = Group()
-        for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.ai_game)
-            ship.rect.x = 10 + ship_number * ship.rect.width
-            ship.rect.y = 10
-            self.ships.add(ship)
-
+    def check_edges(self):
+        """Return True if alien is at edge of screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
         
-    def check_high_score(self):
-        """Check to see if there's a new high score."""
-        if self.stats.score > self.stats.high_score:
-            self.stats.high_score = self.stats.score
-            self.prep_high_score()
-        
-    def show_score(self):
-        """Draw scores, level, and ships to the screen."""
-        self.screen.blit(self.score_image, self.score_rect)
-        self.screen.blit(self.high_score_image, self.high_score_rect)
-        self.screen.blit(self.level_image, self.level_rect)
-        self.ships.draw(self.screen)
+    def update(self):
+        """Move the alien right or left"""
+        self.x += (self.settings.alien_speed *
+                  self.settings.fleet_direction)
+        self.rect.x = self.x
